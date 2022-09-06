9e4109093e1af966b5e04a2583e2e82fbcb6999f
KarenVargas <kalivargas.11@gmail.com>
2022-09-06 12:14:16 -0500

    Creacion de clases jugador

---
+++ src/main/java/ModelDAO/JuadorDAO.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModelDAO;
+
+/**
+ * * Esta clase tendra los metodos necesarios para la clase Jugador
+ * @author Karen
+ * @version 1.0
+ */
+public class JuadorDAO {
+    
+}
---
+++ src/main/java/ModelVO/JugadorVO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModelVO;
+
+/**
+ * Esta clase tendra los constructores, get y setter de los datos necesarios 
+ * @author Karen
+ * @version 1.0
+ * 
+ */
+public class JugadorVO {
+    
+}
