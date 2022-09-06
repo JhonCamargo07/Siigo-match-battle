f60148d4014f95f914941c032f5e1bb1fe281ecd
KarenVargas <kalivargas.11@gmail.com>
2022-09-06 10:47:23 -0500

    Documentacion

---
+++ README.md
@@ -0,0 +1,4 @@
+# SIIGO MATCH BATTLE 
+
+![Diagrama de Clases](Diagrama de Clases.png)
+![Mockup index(index.png)
---
+++ src/main/java/com/mycompany/siigo_match_battle/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.siigo_match_battle;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
