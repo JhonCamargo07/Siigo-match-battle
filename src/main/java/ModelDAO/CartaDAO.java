9e1f04a0ef2012c9894c8c6663c98b2d0c0a0e29
Jhon Alexander Camargo <jhonalexcamargo07@gmail.com>
2022-09-06 17:01:00 -0500

    Avances en las constatntes para las cards

--- src/main/java/ModelDAO/CartaDAO.java
+++ src/main/java/ModelDAO/CartaDAO.java
@@ -15,8 +15,13 @@ public class CartaDAO implements ICartaDAO {
 //    private static final int MAX_CARTAS = 12;
     private static final int MIN_CARTAS = 4;
     private static final char[] NUMEROS_CARTAS = {'1', '2', '3', '4'};
-    private static final char[] LETRAS_CARTAS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+    private static final String[] LETRAS_CARTAS = {"A", "C", "D", "E", "F", "G", "H", "J"};
     private static final String[] TITULO_CARTAS = {"Lenovo 2022", "Azus 2015", "Samsung 2020", "Dell 2012", "Huawei 2015", "Acer 2017", "HP 2021", "Toshiba 2022", "Apple 2018", "Apple 2019", "Lenovo 2012", "Toshiba 2018", "Samsung 2021", "Huawei 2014", "Asus 2016", "HP 2020", "Acer ", "Apple 2018"};
+    
+    private static final String[] BOARD_ASUS_CARTAS = {"560", "550", "540", "520", "570", "580", "590", "510", "530", "510", "560", "550", "540", "520", "570", "580", "590", "510", "530", "560", "550", "540", "520", "570", "580", "590", "510", "530", "560", "550", "540", "520"};
+    
+    private static final String[] DISCO_DURO_CARTAS = {"250", "300", "500", "520", "700", "920", "1000", "2000", "2100", "350", "400", "800","250", "300", "500", "520", "700", "920", "1000", "2000", "2100", "350", "400", "800", "250", "300", "500", "520", "700", "920", "1000", "2000",};
+    
 
     @Override
     public List<List<CartaVO>> generarCartas(int cantidadJugadores) {
