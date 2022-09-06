e514fb3379e7f230909f38435599fc6c7451fea6
KarenVargas <kalivargas.11@gmail.com>
2022-09-06 13:53:26 -0500

    Creacion metodos Judador

--- src/main/java/ModelDAO/IJugador.java
+++ src/main/java/ModelDAO/IJugador.java
@@ -4,10 +4,46 @@
  */
 package ModelDAO;
 
+import java.util.List;
+
 /**
  *
- * @author Aprendiz
+ * Esta interface tendra los metodos utilizados en el juego
+ *
+ * @author Karen
+ * @version 1.0
  */
 public interface IJugador {
+
+    /**
+     * Este metodo se encargara de crear la partida del juego
+     *
+     * @return String, retorna el codigo de la partida
+     */
+    public String crearPartida();
+
+    /**
+     * Este metodo se encargara de iniciar la partida del juego una vez ya este
+     * el numero minido de participantes o el maximo
+     */
+    public void iniciarPartida();
+
+    /**
+     * Este metodo validara el ingreso a la Partida
+     *
+     * @param codigoMesa
+     * 
+     * @return boolean, retornara true o false y segun la respuesta se ingresara
+     * o no a la partida
+     *
+     */
+    public boolean ingresarPartida(String codigoMesa);
+
+    /**
+     *Este metodo retornara una lista con la caracteristica seleccionada
+     * @return String, retornara un string con la caracteristica y valor seleccionada
+     */
     
+    public List<String>escogerCaracteristica();
+
 }
